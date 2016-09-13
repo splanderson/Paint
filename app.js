c67b81c817744c38bece68d1b4317e0c1fd1b4a0
Eric Anderson <splanderson@gmail.com>
2016-09-13 16:24:20 -0600

    finished with colors

--- app.js
+++ app.js
@@ -8,4 +8,37 @@ $(document).ready(function() {
   $('#reset').on('click', function() {
     $('.box').removeClass('white')
   })
+  var color = 'white'
+  $('#red').on('click', function() {
+    color = 'red';
+  })
+
+  $('#blue').on('click', function() {
+    color = 'blue';
+  })
+
+  $('#green').on('click', function() {
+    color = 'green';
+  })
+
+  $('#yellow').on('click', function() {
+    color = 'yellow';
+  })
+
+  $('#white').on('click', function() {
+    color = 'white';
+  })
+  $('.box').on('click', function() {
+    $(this).addClass(color);
+  })
+
+  var colors = 'white green red blue yellow'
+
+  $('.box').on('dblclick', function() {
+    $(this).removeClass(colors);
+  })
+
+  $('#reset').on('click', function() {
+    $('.box').removeClass(colors)
+  })
 })
